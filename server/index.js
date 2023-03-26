0390c0abac243e3f3629e6634c31b124f29f7395
Arda Coşkun <m.ardacoskun@gmail.com>
2023-03-26 18:09:09 +0300

    mongodb connected

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Database connection failed.Server not started.");
+    console.log(error);
+  });
