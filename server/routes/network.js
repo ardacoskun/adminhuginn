5f19a2acce6fdd3e2b4e1830592306efa0ff23dd
Arda Coşkun <m.ardacoskun@gmail.com>
2023-04-20 13:58:32 +0300

    verifyToken error

--- server/routes/network.js
+++ server/routes/network.js
@@ -9,18 +9,23 @@ const {
 const { verifyTokenAndAdmin } = require("../middleware/verifyToken");
 
 //GET ALL NETWORKS
-router.get("/", verifyTokenAndAdmin, getAllNetworks);
+// router.get("/", verifyTokenAndAdmin, getAllNetworks);
+router.get("/", getAllNetworks);
 
 //CREATE NETWORK
-router.post("/create", verifyTokenAndAdmin, createNetwork);
+// router.post("/create", verifyTokenAndAdmin, createNetwork);
+router.post("/create", createNetwork);
 
 //GET NETWORK
-router.get("/:id", verifyTokenAndAdmin, getNetwork);
+// router.get("/:id", verifyTokenAndAdmin, getNetwork);
+router.get("/:id", getNetwork);
 
 //UPDATE NETWORK
-router.put("/:id", verifyTokenAndAdmin, updateNetwork);
+// router.put("/:id", verifyTokenAndAdmin, updateNetwork);
+router.put("/:id", updateNetwork);
 
 //DELETE NETWORK
-router.delete("/:id", verifyTokenAndAdmin, deleteNetwork);
+// router.delete("/:id", verifyTokenAndAdmin, deleteNetwork);
+router.delete("/:id", deleteNetwork);
 
 module.exports = router;
